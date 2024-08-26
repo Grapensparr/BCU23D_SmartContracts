d6904e8009cde17adc15f5d2a64fcf26619ea411
Liza Grapensparr <113124538+Grapensparr@users.noreply.github.com>
2024-08-26 14:58:27 +0200

    Contract added

---
+++ Lektion 4/AccessControl.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract AccessControl {
+    mapping(address => bool) public admins;
+    mapping(address => bool) public supporters;
+    mapping(address => bool) public members;
+
+    event RoleAssigned(address indexed account, string role);
+
+    constructor() {
+        admins[msg.sender] = true;
+        emit RoleAssigned(msg.sender, "Admin");
+    }
+
+    modifier onlyAdmin() {
+        require(admins[msg.sender], "You are not an admin and cannot call this function");
+        _;
+    }
+
+    function assignRole(address _account) public onlyAdmin {
+        admins[_account] = true;
+        emit RoleAssigned(_account, "Admin");
+    }
+
+    function assignRole(address _account, string memory _role) public onlyAdmin {
+        if (keccak256(bytes(_role)) == keccak256(bytes("Supporter"))) {
+            supporters[_account] = true;
+        } else if (keccak256(bytes(_role)) == keccak256(bytes("Member"))) {
+            members[_account] = true;
+        } else {
+            revert("Invalid role. Try again!");
+        }
+
+        emit RoleAssigned(_account, _role);
+    }
+}
